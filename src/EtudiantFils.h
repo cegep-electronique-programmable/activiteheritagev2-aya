4879b3480afbd0cf59ca6c6284e2a4d8a9e4b491
yasss14 <yahyaouiyassine05@gmail.com>
2025-05-13 10:41:13 -0400

    class TGE

--- src/EtudiantFils.h
+++ src/EtudiantFils.h
@@ -4,3 +4,28 @@
 #include <string>
 #include <iostream>
 #include "Etudiant.h"
+
+// Classe TGE
+class EtudiantTGE : public Etudiant {
+private:
+    bool maitriseOscillo;
+
+public:
+    void setMatricule(int matricule) {
+        if (std::to_string(matricule)[0] == '2') {
+            Etudiant::setMatricule(matricule);
+        } else {
+            std::cout << "Erreur: Le matricule TGE doit commencer par 2." << std::endl;
+        }
+    }
+
+    void setMaitriseOscillo(bool valeur) {
+        maitriseOscillo = valeur;
+    }
+
+    bool getMaitriseOscillo() {
+        return maitriseOscillo;
+    }
+};
+
+#endif
